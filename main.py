d3a09d021c4c6fc38d807394da0069577ebcb5d7
Matthew Campbell <m.campbell92@gmail.com>
2025-10-19 15:54:24 +0200

    write callback endpoint

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import secrets
 import string
 import hashlib
 import base64
+import requests
 from urllib.parse import urlencode
 from fastapi import FastAPI, Response, Request, HTTPException
 from fastapi.responses import RedirectResponse
@@ -11,8 +12,10 @@ app = FastAPI()
 
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-REDIRECT_URI = os.environ["URI"] + "/callback"
-
+STATE_KEY = "spotify_auth_state"
+CODE_VERIFIER_KEY = "spotify_code_verifier"
+URI = os.environ["URI"]
+REDIRECT_URI = URI + "/callback"
 
 def generate_state_string(length=20):
     characters = string.ascii_letters + string.digits
@@ -23,7 +26,7 @@ def generate_code_verifier(length=128):
     return "".join(secrets.choice(characters) for _ in range(length))
 
 def generate_pkce(code_verifier: str) -> str:
-    sha256_hash = hashlib.sha256(code_verifier.encode("ut-8")).digest()
+    sha256_hash = hashlib.sha256(code_verifier.encode("utf-8")).digest()
     code_challenge = base64.urlsafe_b64encode(sha256_hash).decode("utf-8").rstrip("=")
     return code_challenge
 
@@ -38,8 +41,8 @@ def handle_login( response: Response):
     code_verifier = generate_code_verifier()
     code_challenge = generate_pkce(code_verifier)
     
-    response.set_cookie(key="code_verifier", value=code_verifier)
-    response.set_cookie(key="oauth_state", value=state_string)
+    response.set_cookie(key=STATE_KEY, value=state_string)
+    response.set_cookie(key=CODE_VERIFIER_KEY, value=code_verifier)
 
     params = {
         "client_id": client_id,
@@ -56,3 +59,47 @@ def handle_login( response: Response):
     response = RedirectResponse(url=auth_url)
 
     return response
+
+@app.get("/callback")
+def handle_callback(request: Request, response: Response):
+    client_id = CLIENT_ID
+    grant_type = "authorization_code"
+    spotify_code = request.query_params["code"]
+    redirect_uri = REDIRECT_URI
+    state = request.query_params["state"]
+    stored_state = request.cookies.get(STATE_KEY)
+    code_verifier = request.cookies.get(CODE_VERIFIER_KEY)
+    token_endpoint = "https://accounts.spotify.com/api/token"
+
+    if state == None or state != stored_state:
+        raise HTTPException(status_code=400, detail="State key mismatch.")
+    else:
+        response.delete_cookie(STATE_KEY)
+        response.delete_cookie(CODE_VERIFIER_KEY)
+        params = {
+            "client_id": client_id,
+            "grant_type": grant_type,
+            "code": spotify_code,
+            "redirect_uri": redirect_uri,
+            "code_verifier": code_verifier
+        }
+
+        api_response = requests.post(token_endpoint, data=params)
+
+        if api_response.status_code == 200:
+            result = api_response.json()
+            access_token = result["access_token"]
+            refresh_token = result["refresh_token"]
+
+            response = RedirectResponse(url=URI)
+            response.set_cookie(key="access_token", value=access_token)
+            response.set_cookie(key="refresh_token", value=refresh_token)
+        else:
+            raise HTTPException(status_code=api_response.status_code, detail="Token exchange failed.")
+
+        return response
+
+@app.get("/refresh_token")
+def refresh_token(request: Request):
+    refresh_token = request.cookies.get("refresh_token")
+    request_string = CLIENT_ID + ":" + CLIENT_SECRET
