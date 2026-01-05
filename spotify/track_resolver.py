61b68c2f312935eb9c43b7d7db92c63f67fa0641
Matthew Campbell <m.campbell92@gmail.com>
2026-01-05 23:12:17 +0100

    add caching logic

--- spotify/track_resolver.py
+++ spotify/track_resolver.py
@@ -1,10 +1,15 @@
 import requests, random
 
+from spotify.cache import ARTIST_ID_CACHE, TOP_TRACK_CACHE
+
 def resolve_artist_id(artist_name, access_token):
+    key = artist_name.lower().strip()
+
+    if key in ARTIST_ID_CACHE:
+        return ARTIST_ID_CACHE[key]
+
     endpoint = "https://api.spotify.com/v1/search"
-
     headers = {"Authorization": f"Bearer {access_token}"}
-
     params = {
         "q": artist_name,
         "type": "artist",
@@ -14,31 +19,31 @@ def resolve_artist_id(artist_name, access_token):
     response = requests.get(endpoint, headers=headers, params=params)
 
     if response.status_code != 200:
-        print("Spotify error:", response.text)
-        return
+        ARTIST_ID_CACHE[key] = None
+        return None
     
     data = response.json()
 
     items = data["artists"]["items"]
     if not items:
+        ARTIST_ID_CACHE[key] = None
         return None
 
     artist_id = items[0]["id"]
+    ARTIST_ID_CACHE[key] = artist_id
 
-    print(response.status_code)
-    print(response.json())
-    print(artist_id)
     return artist_id
 
 
 
 def resolve_artist_to_track_uri(artist_id, access_token):
+    if artist_id in TOP_TRACK_CACHE:
+        return TOP_TRACK_CACHE[artist_id]
+
+    endpoint = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
-
-    endpoint = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
-
     params = {
         "market": "US"
     }
@@ -53,10 +58,12 @@ def resolve_artist_to_track_uri(artist_id, access_token):
     tracks = data.get("tracks", [])
 
     if not tracks:
+        TOP_TRACK_CACHE[artist_id] = None
         return None
 
-    track = random.choice(tracks)
-    return track["uri"]
+    uri = random.choice(tracks)["uri"]
+    TOP_TRACK_CACHE[artist_id] = uri
+    return uri
 
 
 
