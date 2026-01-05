c3fa41d12d309f6cfbc1be35ac5489204df0a3d9
Matthew Campbell <m.campbell92@gmail.com>
2026-01-05 16:46:09 +0100

    rename folder

--- playlist/selection.py
+++
@@ -1,6 +0,0 @@
-from db.queries import get_random_festival_year_id, get_random_artists_for_year
-
-def random_artists():
-    festival_year_id = get_random_festival_year_id()
-    random_artists = get_random_artists_for_year(festival_year_id)
-    return random_artists
